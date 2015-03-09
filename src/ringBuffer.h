b2cd94ad51bf077e725c8131ed0422d311e52a01
dvhh <dinh@ifrec.osaka-u.ac.jp>
2015-03-09 17:06:40 +0900

    documentations

--- src/ringBuffer.h
+++ src/ringBuffer.h
@@ -1,14 +1,77 @@
 #pragma once
+/**
+@file
+
+@author DINH Viet Huy Hubert <dinh@ifrec.osaka-u.ac.jp>
+@version 1.0
+@section DESCRIPTION
+
+A general purpose pthread ring buffer queue 
+**/
 #include <stdbool.h>
 
 typedef struct rb_ringBuffer rb_ringBuffer_t;
 
+/**
+Create a new queue 
+
+@param capacity [in] desired queue capacity 
+
+@return newly created queue
+**/
 rb_ringBuffer_t* rb_init(size_t capacity);
+
+/**
+destroy queue
+
+@param queue to destroy 
+
+only destroy the queue, any remaining elements are up to the developper
+**/
 void rb_destroy(rb_ringBuffer_t* buffer);
+
+/**
+enqueue an element in the queue
+
+@param buffer [out] queue to add element in
+@param item [in] queue 
+**/
 void rb_enqueue(rb_ringBuffer_t* buffer,void* item);
+
+/**
+dequeue en element from the queue
+@param buffer queue to get the element from
+@return element dequeued from the queue
+**/
 void* rb_dequeue(rb_ringBuffer_t* buffer);
+
+/**
+return the element count in the list (not thread safe)
+@param buffer queue to get the elements count from
+@return count of elemnt in the queue 
+**/
 size_t rb_count(rb_ringBuffer_t* buffer);
+
+/**
+check if the queue is full 
+@param buffer queue to check
+@return true if the queue is full
+**/
 bool rb_isFull(rb_ringBuffer_t* buffer);
 
+/**
+enqueue an element in the queue (non blocking version)
+
+@param buffer [out] queue to add element in
+@param item [in] queue 
+@return true if the operation succeeded
+**/
 bool rb_enqueueNonBlocking(rb_ringBuffer_t* buffer,void* item);
+
+/**
+dequeue en element from the queue (non blocking version)
+@param buffer queue to get the element from
+@param output [out] pointer to write the dequeued element
+@return true if the operation succeeded
+**/
 bool rb_dequeueNonBlocking(rb_ringBuffer_t* buffer,void** output);
--- src/threadPool.h
+++ src/threadPool.h
@@ -1,10 +1,48 @@
 #pragma once
+/**
+@file
 
+@author DINH Viet Huy Hubert <dinh@ifrec.osaka-u.ac.jp>
+@version 1.0
+@section DESCRIPTION
 
+generic pthread threadpool
+**/
+
+/**
+worker routine definition
+**/
 typedef void (tp_worker_t )(void*);
+
 typedef struct tp_threadpool tp_threadpool_t;
 
+/**
+Create a threadpool
+
+@param count [in] number of threads available in the threadpool
+@return new threadpool created
+**/
 tp_threadpool_t* tp_init(unsigned int count);
+
+/**
+destroy a threadpool 
+
+@param pool pool to be destroyed
+**/
 void tp_destroy(tp_threadpool_t* pool);
+
+/**
+enqueue a work request to the pool
+
+@param pool pool to be used
+@param worker routine to be used for the task
+@param arg task paramters
+**/
 void tp_enqueue(tp_threadpool_t* pool,tp_worker_t* worker,void* arg);
+
+/**
+wait for current outstanding task queue in the pool to finish 
+
+@param pool threadpool to wait task upon
+**/
 void tp_wait(tp_threadpool_t* pool);
